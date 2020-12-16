cc854461ba83d8e1b5250c988cafaa72f387ffed
Thomas Bonk <thomas@meandmymac.de>
2020-12-16 12:50:29 +0100

    updated projects structure and Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,17 @@
 // swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
-    name: "Bayes"
-    )
+  name: "Bayes",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(name: "Bayes", targets: ["Bayes"]),
+  ],
+  dependencies: [  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(name: "Bayes"),
+  ]
+)
